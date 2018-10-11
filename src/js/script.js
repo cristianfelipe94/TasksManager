321012a73814f5d44d8d2f2530567ea79fec4261
Cristian Felipe <cristianpipe0494@gmail.com>
2018-10-11 17:59:41 -0600

    Agrego la funcionalidad en Js, junto con estilos basicos en Sass.

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
+const textNameWord = document.getElementById('taskManager');
+const dateManager = document.getElementById('dateManager');
+const incompletedNameBox = document.getElementById('taskNameIncompletedBox');
+const incompletedDateBox = document.getElementById('taskDateIncompletedBox');
+const completedBox = document.getElementById('taskCompletedBox');
+const mainTitle = document.getElementById('mainTitle');
+const btnAction = document.getElementById('btnAction');
 
+function uploadTask() {
+  if (textNameWord.value !== '' && dateManager.value) {
+    mainTitle.style.color = 'red';
+    const newTaskName = document.createElement('li');
+    const newTaskDate = document.createElement('li');
+    incompletedNameBox.appendChild(newTaskName);
+    incompletedDateBox.appendChild(newTaskDate);
+    newTaskName.innerHTML = textNameWord.value;
+    newTaskDate.innerHTML = dateManager.value;
+    textNameWord.value = '';
+    dateManager.value = '';
+  } else if (textNameWord.value === '' && !dateManager.value) {
+    mainTitle.innerHTML = 'Error ingrese Nombre de Tarea y Fecha.';
+  }
+}
+
+btnAction.addEventListener('click', uploadTask);
