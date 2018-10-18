aafd150779eec31861be5c2a0c897cf205d7731d
Cristian Felipe <cristianpipe0494@gmail.com>
2018-10-18 11:50:04 -0600

    Agrego el contenido al ejercicio de Ajax

--- TaskManagerAjax/src/js/script.js
+++ TaskManagerAjax/src/js/script.js
@@ -1,38 +1,118 @@
-/**
- * Ejemplo de AJAX con XMLHttpRequest.
- *
- */
 
-// dataReceived(): Este evento se dispara cuando el request
-// obtiene sus datos de forma satisfactoria.
-function dataReceived(event) {
-  console.log(event.target);
-  const response = event.target.response;
-  const container = document.getElementById('container');
-  for (const element of response) {
-    const listItem = document.createElement('li');
-    listItem.innerText = element.text;
-    listItem.style.color = element.color;
-    if (element.bold) {
-      listItem.style.fontWeight = 'bold';
+// Get all the elements from the DOM.
+const textNameWord = document.getElementById('taskManager');
+const dateManager = document.getElementById('dateManager');
+const incompletedNameBox = document.getElementById('taskNameIncompletedBox');
+const incompletedDateBox = document.getElementById('taskDateIncompletedBox');
+const completedBox = document.getElementById('taskCompletedBox');
+const incompletedNewRow = document.getElementById('newRowIncompletedBox');
+const completedNewRow = document.getElementById('newRowCompletedBox');
+const mainTitle = document.getElementById('mainTitle');
+const btnAction = document.getElementById('btnAction');
+
+// Create an Array.
+const ObjectArray = [];
+
+// Main function, Upload the current value into the DOM.
+function uploadTask() {
+  if (textNameWord.value !== '' && dateManager.value) {
+    // Create the elements that are going to be in the DOM.
+    // Elements that are going to be Created after Event.
+    const newTaskNameList = document.createElement('ul');
+    const newTaskDateList = document.createElement('ul');
+    const newTaskNameListItem = document.createElement('li');
+    const newTaskDateListItem = document.createElement('li');
+    const checkBoxList = document.createElement('input');
+
+    // Information that is going to be inside the DOM Elements.
+    newTaskNameList.appendChild(newTaskNameListItem);
+    newTaskDateList.appendChild(newTaskDateListItem);
+    newTaskDateList.appendChild(checkBoxList);
+    newTaskNameListItem.innerHTML = textNameWord.value;
+    newTaskDateListItem.innerHTML = dateManager.value;
+
+    // Set classes for the DOM Elements
+    newTaskNameList.setAttribute('class', 'incompletedBlock');
+    newTaskDateList.setAttribute('class', 'incompletedBlock');
+    checkBoxList.setAttribute('type', 'checkbox');
+    checkBoxList.setAttribute('class', 'checkBoxStatus');
+
+    // Include the information within the DOM Elements into the respective Lists or Rows.
+    incompletedNewRow.appendChild(newTaskNameList);
+    incompletedNewRow.appendChild(newTaskDateList);
+
+    // Create an Object with information in it.
+    const MainArray = ({ TaskName: textNameWord.value, DateData: dateManager.value, Completed: false });
+
+    // Add the Object into the Array.
+    ObjectArray.push(MainArray);
+    console.log(ObjectArray);
+
+    // Apply styles to the Incompleted Rows.
+    newTaskNameList.style.color = ('red');
+    newTaskDateList.style.color = ('red');
+
+    // Clear all the Set information for new options.
+    textNameWord.value = '';
+    dateManager.value = '';
+    textNameWord.style.border = ('');
+    dateManager.style.border = ('');
+
+    // Message that will be displayed as Task is successfully registered.
+    mainTitle.innerHTML = 'Successful Task Registration.';
+    function changeStatus() {
+      if (checkBoxList.checked) {
+
+        //Will change Status from Object.
+        MainArray.Completed = true;
+        console.log(ObjectArray);
+
+        //Set classes to DOM Elements.
+        newTaskNameList.setAttribute('class', 'completedBlock');
+        newTaskDateList.setAttribute('class', 'completedBlock');
+
+        //Information included into DOM Elements.
+        completedNewRow.appendChild(newTaskNameList);
+        completedNewRow.appendChild(newTaskDateList);
+
+        //DOM Elements Styles.
+        newTaskNameList.style.color = ('green');
+        newTaskDateList.style.color = ('green');
+      } else if (!checkBoxList.checked) {
+
+        //Will change Status from Object.
+        MainArray.Completed = false;
+        console.log(ObjectArray);
+
+        //Set classes to DOM Elements.
+        newTaskNameList.setAttribute('class', 'incompletedBlock');
+        newTaskDateList.setAttribute('class', 'incompletedBlock');
+
+        //Information included into DOM Elements.
+        incompletedNewRow.appendChild(newTaskNameList);
+        incompletedNewRow.appendChild(newTaskDateList);
+
+        //DOM Elements Styles.
+        newTaskNameList.style.color = ('red');
+        newTaskDateList.style.color = ('red');
+      }
     }
-    container.appendChild(listItem);
+
+    checkBoxList.addEventListener('click', changeStatus);
+  } else if (textNameWord.value === '' && !dateManager.value) {
+    textNameWord.style.border = ('5px solid red');
+    dateManager.style.border = ('5px solid red');
+    mainTitle.innerHTML = 'Error, please complete the following information.';
+  } else if (textNameWord.value && !dateManager.value) {
+    textNameWord.style.border = ('');
+    dateManager.style.border = ('5px solid red');
+    mainTitle.innerHTML = 'Error, please complete the following information.';
+  } else if (!textNameWord.value && dateManager.value) {
+    textNameWord.style.border = ('5px solid red');
+    dateManager.style.border = ('');
+    mainTitle.innerHTML = 'Error, please complete the following information.';
   }
 }
 
-// getElements(): Crea un nuevo request para solicitar los datos.
-function getElements() {
-  // Crea un nuevo XMLHttpRequest.
-  const request = new XMLHttpRequest();
-  // Agrega un event listener para el evento 'load'.
-  request.addEventListener('load', dataReceived);
-  // Define el tipo de respuesta esperada como 'json'.
-  request.responseType = 'json';
-  // Inicializa el request.
-  request.open('GET', '/data.json');
-  // Envía el request.
-  request.send();
-}
-
-const button = document.getElementById('load-elements');
-button.addEventListener('click', getElements);
+// Set the Event to the BTN to run the Function.
+btnAction.addEventListener('click', uploadTask);
--- TaskManagerSimple/src/js/script.js
+++ TaskManagerSimple/src/js/script.js
@@ -10,55 +10,55 @@ const completedNewRow = document.getElementById('newRowCompletedBox');
 const mainTitle = document.getElementById('mainTitle');
 const btnAction = document.getElementById('btnAction');
 
+// Create an Array.
+const ObjectArray = [];
+
 // Main function, Upload the current value into the DOM.
 function uploadTask() {
   if (textNameWord.value !== '' && dateManager.value) {
-    //Create the elements that are going to be in the DOM.
-    //Elements that are going to be Created after Event.
+    // Create the elements that are going to be in the DOM.
+    // Elements that are going to be Created after Event.
     const newTaskNameList = document.createElement('ul');
     const newTaskDateList = document.createElement('ul');
     const newTaskNameListItem = document.createElement('li');
     const newTaskDateListItem = document.createElement('li');
     const checkBoxList = document.createElement('input');
 
-    //Information that is going to be inside the DOM Elements.
+    // Information that is going to be inside the DOM Elements.
     newTaskNameList.appendChild(newTaskNameListItem);
     newTaskDateList.appendChild(newTaskDateListItem);
     newTaskDateList.appendChild(checkBoxList);
     newTaskNameListItem.innerHTML = textNameWord.value;
     newTaskDateListItem.innerHTML = dateManager.value;
 
-    //Set classes for the DOM Elements
+    // Set classes for the DOM Elements
     newTaskNameList.setAttribute('class', 'incompletedBlock');
     newTaskDateList.setAttribute('class', 'incompletedBlock');
     checkBoxList.setAttribute('type', 'checkbox');
     checkBoxList.setAttribute('class', 'checkBoxStatus');
 
-    //Include the information within the DOM Elements into the respective Lists or Rows.
+    // Include the information within the DOM Elements into the respective Lists or Rows.
     incompletedNewRow.appendChild(newTaskNameList);
     incompletedNewRow.appendChild(newTaskDateList);
 
-    //Create an Array.
-    const ObjectArray = [];
-
-    //Create an Object with information in it.
+    // Create an Object with information in it.
     const MainArray = ({ TaskName: textNameWord.value, DateData: dateManager.value, Completed: false });
 
-    //Add the Object into the Array.
+    // Add the Object into the Array.
     ObjectArray.push(MainArray);
     console.log(ObjectArray);
 
-    //Apply styles to the Incompleted Rows.
+    // Apply styles to the Incompleted Rows.
     newTaskNameList.style.color = ('red');
     newTaskDateList.style.color = ('red');
 
-    //Clear all the Set information for new options.
+    // Clear all the Set information for new options.
     textNameWord.value = '';
     dateManager.value = '';
     textNameWord.style.border = ('');
     dateManager.style.border = ('');
 
-    //Message that will be displayed as Task is successfully registered.
+    // Message that will be displayed as Task is successfully registered.
     mainTitle.innerHTML = 'Successful Task Registration.';
     function changeStatus() {
       if (checkBoxList.checked) {
@@ -97,6 +97,7 @@ function uploadTask() {
         newTaskDateList.style.color = ('red');
       }
     }
+
     checkBoxList.addEventListener('click', changeStatus);
   } else if (textNameWord.value === '' && !dateManager.value) {
     textNameWord.style.border = ('5px solid red');
